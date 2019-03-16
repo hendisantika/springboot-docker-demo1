fef839cf27643c55c7fa1821fd79c7d9ede85ca9
hendisantika <hendisantika@yahoo.co.id>
2019-03-16 11:00:50 +0700

    #6 - Modify Main Class

--- src/main/java/com/hendisantika/springbootdockerdemo1/SpringbootDockerDemo1Application.java
+++ src/main/java/com/hendisantika/springbootdockerdemo1/SpringbootDockerDemo1Application.java
@@ -1,13 +1,33 @@
 package com.hendisantika.springbootdockerdemo1;
 
+import com.hendisantika.springbootdockerdemo1.model.Employee;
+import com.hendisantika.springbootdockerdemo1.repository.EmployeeRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringbootDockerDemo1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootDockerDemo1Application.class, args);
+    }
+
+    //for testing only
+    @Bean
+    public CommandLineRunner demo(EmployeeRepository repository) {
+        return (args) -> {
+
+            Employee employee = new Employee();
+            employee.setId("1");
+            employee.setFullName("Uzumaki Naruto");
+            employee.setEmail("uzumaki_naruto@gmail.com");
+            employee.setManagerEmail("sarutobi@gmail.com");
+
+            repository.save(employee);
+
+        };
     }
 
 }
